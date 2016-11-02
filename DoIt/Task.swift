654363f32fef538bfbcefc8e9196084f90ee9160
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-02 18:08:22 +0100

    Added Task class, navigation, viewController and segue

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Thomas Bentkowski on 02/11/2016.
+//  Copyright © 2016 Thomas Bentkowski. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
